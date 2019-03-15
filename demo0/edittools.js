cc0987059252ccd7c5f19764b0b8096fbba86aae
chenli <chenli@laka-inc.com>
2019-03-15 15:21:36 +0800

    添加对图片资源的处理

--- demo0/edittools.js
+++ demo0/edittools.js
@@ -2,20 +2,28 @@
  * @returns 修改后的html文件
  * @description 修改html文件中js,css的版本号
  */
+/**
+ * @returns 修改后的html文件
+ * @description 修改html文件中js,css的版本号
+ */
 var fs = require('fs')
 var crypto = require('crypto')
+var path = require('path')
 var debug = true
 var md5_path = './fileList.json' // 记录js，css文件名及其MD5值(此文件内容勿随意删除)
 var js_root_path = './dist/js'  //存放js文件的根目录
-var css_root_path = './dist/css' // 存放css文件的根目录
+var css_root_path = './css' // 存放css文件的根目录
 var html_root_path = './' // 存放html文件的根目录
+var img_root_path = 'img' //存放图片资源根目录
 
 var fileList; // 存储md5_path文件的内容
 var js_files; // 存储js_root_path目录下的所有js文件列表
 var css_files; // 存储css_root_path目录下的所有css文件列表
 var html_files; //存储html_root_path目录下的所有html文件列表
-var new_md5_json; // 存储处理后的文件MD5值列表（作用：与fileList进行比较，同时更新md5_path文件）
+var new_md5_json = Object.create(null); // 存储处理后的文件MD5值列表（作用：与fileList进行比较，同时更新md5_path文件）
+new_md5_json['images'] = Object.create(null); // 存储处理后的图片MD5值列表
 var editList; // 返回结果
+
 // 文件操作功能函数
 var dealFile = {
   rFile: function(path){
@@ -42,52 +50,75 @@ var dealFile = {
     }
     return;
   },
-  md5File: function(path){
-    var data = this.rFile(path)
+  md5File: function(data){
     return crypto.createHash('md5').update(data, 'utf8').digest('hex');
   },
   dealreg: function(name){
-    let reg = '\/'+ name.replace('.','\\.') +'\.*"'
-    return new RegExp(reg)
+    let reg = '\\W'+name.replace(/\//g,'\/').replace('.','\\.') +'(|\\?v=\.*~)"';
+    return new RegExp(reg, 'g')
   },
   createElist:function(elist, name, type, md5){
     if(md5){
-      elist[name] = {}
+      elist[name] = Object.create(null) //创建空对象
       elist[name]['type'] = type
       elist[name]['md5'] = md5
     }
+  },
+  // 递归获取所有图片文件
+  getPic: function(p){
+    var fls = fs.readdirSync(p)
+    fls.forEach(item => {
+      let src = path.join(p, item);
+      let tmp = fs.lstatSync(src)
+      let t = tmp && tmp.isDirectory() ? 1 : tmp && tmp.isFile() ? 2 : 0;
+      switch(t){
+        case 2:
+          if(/(\.png|\.jpg|\.gif|\.jpeg)$/i.test(item)){
+            new_md5_json['images'][src] = dealFile.md5File(fs.readFileSync(src))
+          }
+          break;
+        case 1: this.getPic(src);break;
+        case 0: break;
+        default: break;
+      }
+    })
   }
 }
 
 fileList = dealFile.rFile(md5_path) ? JSON.parse(dealFile.rFile(md5_path)) : '';
 js_files = dealFile.rDir(js_root_path)
 css_files = dealFile.rDir(css_root_path)
-new_md5_json = {}
-editList = {}
+editList = Object.create(null)
+dealFile.getPic(img_root_path)
 
 // 遍历单目录所有js、css文件，获取并保存MD5值（这里可以优化：递归目录；去除压缩文件）
 js_files.forEach(item => {
   if(item.indexOf('.js') > 0){
     let fname = item.split('.js')[0]
     if(!(fname in new_md5_json)){
-      new_md5_json[fname] = {}
+      new_md5_json[fname] = Object.create(null)
     }
-    new_md5_json[fname]['js'] = dealFile.md5File(js_root_path+'/'+item)
+    new_md5_json[fname]['js'] = dealFile.md5File(dealFile.rFile(js_root_path+'/'+item))
   }
 });
 css_files.forEach(item => {
   if(item.indexOf('.css') > 0){
     let fname = item.split('.css')[0]
     if(!(fname in new_md5_json)){
-      new_md5_json[fname] = {}
+      new_md5_json[fname] = Object.create(null)
     }
-    new_md5_json[fname]['css'] = dealFile.md5File(css_root_path+'/'+item)
+    new_md5_json[fname]['css'] = dealFile.md5File(dealFile.rFile(css_root_path+'/'+item))
   }
 })
+
 // 对新旧文件MD5值进行对比
+var newKeys = Object.keys(new_md5_json).filter(item => {return item !== 'images'})
+var flKeys = Object.keys(fileList).filter(item => {return item !== 'images'})
+var newimgKeys = new_md5_json['images'] ? Object.keys(new_md5_json['images']) : [];
+var flimgKeys = fileList['images'] ? Object.keys(fileList['images']) : [];
 if(fileList){
   let tmp = []
-  Object.keys(new_md5_json).forEach(itemobj => {
+  newKeys.forEach(itemobj => {
     if(itemobj in fileList){
       Object.keys(new_md5_json[itemobj]).forEach(item => {
         if(new_md5_json[itemobj][item] !== fileList[itemobj][item]){
@@ -101,28 +132,46 @@ if(fileList){
       })
     }
   })
-  Object.keys(fileList).forEach(item => {
+  flKeys.forEach(item => {
     Object.keys(fileList[item]).forEach(i => {
       if(tmp.indexOf(item + '.' + i) < 0){
         dealFile.createElist(editList, item +'.'+i, 'del', fileList[item][i])
       }
     })
   })
+  // 单独处理图片
+  newimgKeys.forEach(item => {
+    if(flimgKeys.indexOf(item) < 0){
+      dealFile.createElist(editList, item.replace(/\\/g, '/'), 'add', new_md5_json['images'][item])
+    }else{
+      if(new_md5_json['images'][item] !== fileList['images'][item]){
+        dealFile.createElist(editList, item.replace(/\\/g, '/'), 'edit', new_md5_json['images'][item])
+      }
+    }
+  })
+  flimgKeys.forEach(item => {
+    if(item && newimgKeys.indexOf(item) < 0){
+      dealFile.createElist(editList, item.replace(/\\/g, '/'), 'del', new_md5_json['images'][item])
+    }
+  })
 }else{
-  Object.keys(new_md5_json).forEach(item => {
+  newKeys.forEach(item => {
     dealFile.createElist(editList, item+'.js', 'add', new_md5_json[item]['js'])
     dealFile.createElist(editList, item+'.css', 'add', new_md5_json[item]['css'])
+  })
+  newimgKeys.forEach(item => {
+    dealFile.createElist(editList, item.replace(/\\/g, '/'), 'add', new_md5_json['images'][item])
   })
 }
 // 更新文件MD5列表并返回结果
 dealFile.wFile(md5_path, JSON.stringify(new_md5_json))
 
-// 修改相应html文件
+// 修改相应html文件需要的参数
 var keys = Object.keys(editList);
 var reg_arr = (function(){
   let arr = []
   keys.forEach(item => {
-    let obj = {}
+    let obj = Object.create(null)
     if(editList[item]['type'] !== 'del'){
       obj['name'] = item
       obj['reg'] = dealFile.dealreg(item)
@@ -155,9 +204,17 @@ if(html_files && reg_arr.length){
     if(item.indexOf('.html')>0){
       var data = dealFile.rFile(html_root_path + item)
       reg_arr.forEach(reg => {
-        if(data.match(reg['reg'])){
-          data = data.replace(reg['reg'], '/'+reg['name']+'?v='+reg['md5']+'"')
-        };
+        var match = data.match(reg['reg'])
+        if(match){
+          console.log(item)
+          console.log(reg['name'])
+          console.log(match)
+          for(let i = 0; i < match.length; i++){
+            let n = match[i].split('?v=')[0];
+            let restr = n[0] === '"' ? '"'+n.replace(/"/g,'')+'?v='+reg['md5']+'~"' : n.replace('"','')+'?v='+reg['md5']+'~"'
+            data = data.replace(match[i], restr)
+          }
+        }
       })
       dealFile.wFile(item, data)
     }
